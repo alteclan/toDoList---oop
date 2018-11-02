0c00278ba43362f57ee8a6337d3e9a60ba3e1d1a
Alteclan <mizamojski@gmail.com>
2018-11-02 18:09:24 +0100

    Clear form after add task

--- toDoList/js/app.js
+++ toDoList/js/app.js
@@ -6,23 +6,32 @@ function Task(task) {
 function UI() {}
 
 // UI add task
-UI.prototype.addTask = function(taskMessage) {
-const tr = document.createElement('tr');
-const table = document.querySelector('.task-list');
-tr.innerHTML = `
-<td>${taskMessage}</td>
-<td>Dzisiejsza data</td>
-<td><i class="fas fa-trash"></i></td>`;
-table.appendChild(tr);
-
+UI.prototype.addTask = function(task) {
+    const tr = document.createElement('tr');
+    const table = document.querySelector('.task-list');
+    tr.innerHTML = `
+    <td>${task.task}</td>
+    <td>Dzisiejsza data</td>
+    <td><i class="fas fa-trash"></i></td>`;
+    table.appendChild(tr);
 }
+
+// UI clear form after add task
+UI.prototype.clearForm = function() {
+    const task = document.querySelector('#toDoMessage');
+    task.value = '';
+}
+
+
 // Event listeners
 document.addEventListener('DOMContentLoaded', function(){
     const form = document.querySelector('.todo-form');
     const ui = new UI();
     form.addEventListener('submit', function(e) {
         const taskMessage = document.querySelector('#toDoMessage').value;
+        const task = new Task(taskMessage);
         e.preventDefault();
-        ui.addTask(taskMessage);
+        ui.addTask(task);
+        ui.clearForm();
     });
 });
