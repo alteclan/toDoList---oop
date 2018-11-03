3ce835e9f3c21be22028b57d8120c4b3c608a996
Alteclan <mizamojski@gmail.com>
2018-11-03 10:38:50 +0100

    Delete task from list

--- toDoList/js/app.js
+++ toDoList/js/app.js
@@ -8,7 +8,7 @@ function UI() {}
 // UI add task
 UI.prototype.addTask = function(task) {
     const tr = document.createElement('tr');
-    const table = document.querySelector('.task-list');
+    const table = document.querySelector('.task-list__body');
     tr.innerHTML = `
     <td>${task.task}</td>
     <td>Dzisiejsza data</td>
@@ -22,16 +22,27 @@ UI.prototype.clearForm = function() {
     task.value = '';
 }
 
-
+// UI delete task
+UI.prototype.deleteTask = function(target) {
+    if(target.className === 'fas fa-trash') {
+        target.parentElement.parentElement.remove();
+    }
+}
 // Event listeners
 document.addEventListener('DOMContentLoaded', function(){
     const form = document.querySelector('.todo-form');
+    const tableElement = document.querySelector('.task-list__body');
     const ui = new UI();
+    // Form Event Listener
     form.addEventListener('submit', function(e) {
         const taskMessage = document.querySelector('#toDoMessage').value;
         const task = new Task(taskMessage);
         e.preventDefault();
         ui.addTask(task);
         ui.clearForm();
+    });
+    // Remove Task Event Listener
+    tableElement.addEventListener('click', function(e){
+        ui.deleteTask(e.target);
     });
 });
